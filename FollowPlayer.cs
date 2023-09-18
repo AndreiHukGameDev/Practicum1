a01dc071d94788a5d835cc1e1b5dd7cba8b987b1
AndreiHuk_AutoTesting <belakolala@mail.ru>
2023-09-19 01:01:15 +0300

    Добавьте файлы проекта.

---
+++ FollowPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private float speed = 2.5f;
+    public GameObject firstMarker;
+    public GameObject player;
+    
+    private Vector3 offset = new Vector3 (0, 5, -10);
+
+    
+    void LateUpdate()
+    {
+        try
+        {
+            transform.position = firstMarker.transform.position;
+
+            firstMarker.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            if (transform.position.z >= -20f)
+            {
+
+                Destroy(GameObject.Find("Cube"));
+            }
+        }
+        catch (System.Exception)
+        {
+
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
